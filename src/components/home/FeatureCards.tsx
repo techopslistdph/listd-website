c28396787376e590cfd2d06ec63b98d4a558a3ab
Maricel Montuya <montuyamaricela@gmail.com>
2025-07-21 16:29:46 +0800

    fix(feature cards): Add section title and sub heading, and adjust font sizes, weight and color

--- src/components/home/FeatureCards.tsx
+++ src/components/home/FeatureCards.tsx
@@ -14,6 +14,15 @@ export interface FeatureCardData {
 export default function FeatureCards({ cards }: { cards: FeatureCardData[] }) {
   return (
     <Container>
+      <div className='flex flex-col gap-2 items-center text-center mb-20'>
+        <h2 className='font-bold heading-2 text-primary-main  text-xl lg:text-2xl'>
+          The new way to find your new home
+        </h2>
+        <p className='text-body text-neutral-text text-sm lg:text-base'>
+          Find your dream place to live in with more than 100k+ properties
+          listed.
+        </p>
+      </div>
       <div className='w-full flex flex-col gap-10 md:gap-44 py-8 md:py-12'>
         {cards.map((card, idx) => (
           <div
@@ -33,15 +42,17 @@ export default function FeatureCards({ cards }: { cards: FeatureCardData[] }) {
               />
             </div>
             <div className='bg-white border-[#E0E0E0] border rounded-2xl p-4 md:p-6 w-full md:w-1/2 relative z-10 -mt-40 md:-mt-56 md:-ml-32 md:-mr-32'>
-              <div className='flex flex-col mb-3 md:mb-4'>
-                <span className='w-10 md:w-12'>
+              <div className='flex flex-col mb-1'>
+                <span className='w-10 md:w-16'>
                   <Image src={Icon} alt='' />
                 </span>
                 <h3 className='font-bold text-lg md:text-2xl text-neutral-text mt-2'>
                   {card.title}
                 </h3>
               </div>
-              <p className='text-body text-neutral-mid'>{card.description}</p>
+              <p className='text-base text-neutral-main font-medium'>
+                {card.description}
+              </p>
             </div>
           </div>
         ))}
